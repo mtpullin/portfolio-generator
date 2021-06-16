6cc76a6039241acd1c191f0f815a740fb1b719a3
mtpullin <mtpullin@gmail.com>
2021-06-15 21:20:44 -0400

    moved functions to seperate js files

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
 console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i+= 1){
-    console.log(profileDataArr[i]);
-}
+const [name, github] = profileDataArgs;
 
-console.log('===============');
+console.log(name, github)
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github)
+
+fs.writeFile('index.html', pageHTML, err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
