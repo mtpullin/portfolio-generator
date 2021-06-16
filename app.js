aebe72f33c9981a3b5fa48ac5ea6154f63a8588b
mtpullin <mtpullin@gmail.com>
2021-06-16 10:29:17 -0400

    adding question prompts

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template');
+const inq = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inq
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+.then(answers => console.log(answers));
+// const fs = require('fs')
+// const generatePage = require('./src/page-template');
 
-console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github)
 
-console.log(name, github)
+// fs.writeFile('index.html', pageHTML, err => {
+//     if(err) throw err;
 
-const pageHTML = generatePage(name, github)
-
-fs.writeFile('index.html', pageHTML, err => {
-    if(err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
